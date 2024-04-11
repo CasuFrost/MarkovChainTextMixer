17859694abd834e8d83f97e8f1303bd3a663fcde
Marco Casu <mcasu299@gmail.com>
2024-04-11 14:01:44 +0200

    a

--- headers/structures.h
+++ headers/structures.h
@@ -1,6 +1,6 @@
 
 #define WORD_LENGHT 32 /*30 byte per la parola, 1 byte per un eventuale apostrofo, il byte finale per '\0' */
-
+#define WORDS_TYPE_SIZE 40
 struct wordFrequency
 {
     char word[WORD_LENGHT];
@@ -12,3 +12,15 @@ struct words
     char word[WORD_LENGHT];
     struct wordFrequency *near;
 };
+
+struct words *fileWords;
+int size = 0;
+void initWordsArray()
+{
+    fileWords = malloc(40);
+}
+void addWord(struct words w)
+{
+    fileWords[size * WORDS_TYPE_SIZE] = w;
+    size++;
+}
---
+++ tmp.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/fcntl.h>
+
+// Headers personali
+// #include "headers/ioOperation.h"
+#include "headers/structures.h"
+
+int main()
+{
+    struct words a;
+    printf("%d", sizeof(a));
+}
