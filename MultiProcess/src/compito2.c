a72186667f16d868e13a808d1856e5609883109c
casufrost <mcasu299@gmail.com>
2024-04-25 22:28:26 +0200

    multiProcess

---
+++ MultiProcess/src/compito2.c
@@ -0,0 +1,293 @@
+#include "../headers/compito2.h"
+
+struct Node
+{
+    char word[WORD_LENGHT];
+    int nearSize;
+    int nodeId;
+    struct Edge *edges;
+};
+
+struct Edge
+{
+    struct Node *node;
+    float weight;
+};
+
+int nodesSize = 0;
+struct Node *nodes;
+
+void addNearId(int nodeFrom, int nodeTo, float w)
+{
+    nodes[nodeFrom].nearSize++;
+    if (nodes[nodeFrom].nearSize == 1)
+    {
+        nodes[nodeFrom].edges = calloc(1, (sizeof(struct Edge)));
+    }
+    else
+    {
+        nodes[nodeFrom].edges = realloc(nodes[nodeFrom].edges, nodes[nodeFrom].nearSize * (sizeof(struct Edge)));
+    }
+
+    if (nodes[nodeFrom].edges == NULL)
+    {
+        printf("errore nell'allocazione della memoria\n");
+        exit(1);
+    }
+
+    struct Edge e = {&(nodes[nodeTo]), w};
+
+    nodes[nodeFrom].edges[nodes[nodeFrom].nearSize - 1] = e;
+}
+
+void addNear(struct Node *nodeFrom, struct Node *nodeTo, float w)
+{
+    nodeFrom->nearSize++;
+    if (nodeFrom->nearSize == 1)
+    {
+        nodeFrom->edges = calloc(1, (sizeof(struct Edge)));
+    }
+    else
+    {
+        nodeFrom->edges = realloc(nodeFrom->edges, nodeFrom->nearSize * (sizeof(struct Edge)));
+    }
+
+    if (nodeFrom->edges == NULL)
+    {
+        printf("errore nell'allocazione della memoria\n");
+        exit(1);
+    }
+
+    struct Edge e = {nodeTo, w};
+    e.node = nodeTo;
+
+    e.weight = w;
+
+    nodeFrom->edges[nodeFrom->nearSize - 1] = e;
+}
+
+void createEdge(struct Node *node1, struct Node *node2, float w)
+{
+    addNear(node1, node2, w);
+    // addNear(node2, node1, w);
+}
+
+void printGraph()
+{
+    for (int i = 0; i < nodesSize; i++)
+    {
+        printf("\nnodo : %s vicini :", nodes[i].word);
+
+        for (int j = 0; j < nodes[i].nearSize; j++)
+        {
+            printf("%s - %f, ", (nodes[i]).edges[j].node->word, (nodes[i]).edges[j].weight);
+        }
+    }
+}
+
+void createNode(char word[WORD_LENGHT])
+{
+    struct Node newNode;
+    strcpy(newNode.word, word);
+    newNode.nearSize = 0;
+    newNode.nodeId = nodesSize;
+
+    if (nodesSize == 0)
+    {
+        nodes = (struct Node *)calloc(1, sizeof(struct Node));
+    }
+    else
+    {
+        nodes = (struct Node *)realloc(nodes, (nodesSize + 1) * sizeof(struct Node));
+    }
+
+    if (nodes == NULL)
+    {
+        printf("errore nell'allocazione della memoria\n");
+        exit(1);
+    }
+
+    nodes[nodesSize] = newNode;
+    nodesSize++;
+}
+
+int searchIdFromWord(char word[WORD_LENGHT]) /* Questa funzione  data una parola restituisce l'ID (la posizione nell'array) del nodo con quella parola*/
+{
+    for (int i = 0; i < nodesSize; i++)
+    {
+        if (strcmp(nodes[i].word, word) == 0)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+void createEdgeFromWord(int id1, char word2[WORD_LENGHT], float w)
+{
+
+    int id2 = searchIdFromWord(word2);
+
+    if (id1 == -1 || id2 == -1)
+    {
+        return;
+    }
+    createEdge(&nodes[id1], &nodes[id2], w);
+}
+
+void createGraphFromFile(char *fileName)
+{
+    int wordCounter = 0;
+    FILE *fp;
+    fp = fopen(fileName, "r"); /*Apro il file di input*/
+    char line[MAX_LINE_LENGHT];
+
+    /* Nel primo ciclo while creo tutti i nodi*/
+
+    while (fgets(line, MAX_LINE_LENGHT, fp)) // Leggo il file riga per riga
+    {
+
+        char tmp[WORD_LENGHT]; // Controllo la prima parola in questione
+
+        int i = 0;
+
+        while (i < strlen(line)) // Seleziono la prima parola della linea
+        {
+            if (line[i] == ',')
+            {
+                tmp[i] = '\0';
+                break;
+            }
+            tmp[i] = line[i];
+            i++;
+        }
+
+        // 'tmp' contiene la prima parola
+        createNode(tmp);
+    }
+
+    /* Nel secondo ciclo while creo tutti gli archi */
+    fseek(fp, 0, SEEK_SET); /* Torno all'inizio del file */
+
+    char nodeTo[WORD_LENGHT]; // Controllo la prima parola in questione
+    char tmp[WORD_LENGHT];
+    char freq[WORD_LENGHT];
+
+    while (fgets(line, MAX_LINE_LENGHT, fp)) // Leggo il file riga per riga
+    {
+
+        int i = 0;
+
+        while (i < strlen(line)) // Seleziono la prima parola della linea
+        {
+            if (line[i] == ',')
+            {
+                nodeTo[i] = '\0';
+                break;
+            }
+            nodeTo[i] = line[i];
+            i++;
+        }
+        // printf("%s\n", nodeTo);
+
+        i++;
+
+        int j = 0;
+
+        // Tutti gli archi partiranno da 'nodeTo'
+
+        while (i < strlen(line))
+        {
+            if (line[i] == ',')
+            {
+                tmp[j] = '\0';
+                // Dentro 'tmp' ho la parola
+                j = 0;
+                i++;
+                while (line[i] != ',' && i < strlen(line))
+                {
+                    freq[j] = line[i];
+                    i++;
+                    j++;
+                }
+
+                freq[j] = '\0';
+                j = 0;
+
+                createEdgeFromWord(wordCounter, tmp, atof(freq));
+            }
+            else
+            {
+                tmp[j] = line[i];
+
+                j++;
+            }
+            i++;
+        }
+        wordCounter++;
+    }
+}
+
+void compito2(char *input, char *output, char *numParole, char start[WORD_LENGHT]) /* Esistono due versioni del programma, la prima, opera direttamente sul file (efficente per creare poche
+ parole), la seconda, crea un grafo pesato leggendo il CSV, e poi opera sul grafo, l'operazione di creazione del grafo è dispendiosa, ma poi sarà rapido scrivere sul file di output,
+ per questo se le parole son poche verrà effettuato il processo con la prima versione, se invece son tante, verrà creato il grafo.*/
+{
+
+    int Input_Graph_Pipe[2];
+
+    pid_t pid = fork();
+
+    pipe(Input_Graph_Pipe);
+
+    if (pid == 0)
+    {
+        // FIGLIO
+        // close(Input_Graph_Pipe[0]);
+        FILE *fp;
+        fp = fopen(input, "r"); /*Apro il file di input*/
+        char line[MAX_LINE_LENGHT];
+
+        /* Nel primo ciclo while creo tutti i nodi*/
+
+        while (fgets(line, MAX_LINE_LENGHT, fp)) // Leggo il file riga per riga
+        {
+
+            char tmp[WORD_LENGHT]; // Controllo la prima parola in questione
+
+            int i = 0;
+
+            while (i < strlen(line)) // Seleziono la prima parola della linea
+            {
+                if (line[i] == ',')
+                {
+                    tmp[i] = '\0';
+                    break;
+                }
+                tmp[i] = line[i];
+                i++;
+            }
+
+            // printf("sending... %s\n", tmp);
+            write(Input_Graph_Pipe[1], tmp, (strlen(tmp) + 1));
+            // exit(0);
+        }
+
+        close(fp);
+        exit(0);
+    }
+    else
+    {
+        int returnStatus;
+        // waitpid(pid, &returnStatus, 0);
+        //  close(Input_Graph_Pipe[1]);
+
+        int nbytes;
+        char word[WORD_LENGHT + 1];
+
+        nbytes = read(Input_Graph_Pipe[0], word, sizeof(word));
+
+        printf("letto : %d\n", nbytes);
+
+        // PADRE
+    }
+}
---
+++ MultiProcess/src/main.c
@@ -0,0 +1,39 @@
+#include "../headers/rootHeader.h"
+#include "../headers/compito2.h"
+int main(int argc, char *argv[])
+{
+    if (argc < 4) // è necessario passare almeno un file in input ed uno in output
+    {
+        printf("è necessario fornire un file input, un file output,\nbisogna anche specificare se si vuole eseguire il compito 1 o 2.\n");
+        exit(1);
+    }
+
+    if (strcmp(argv[3], "1") == 0)
+    {
+        // compito1(argv[1], argv[2]);
+    }
+
+    else if (strcmp(argv[3], "2") == 0)
+    {
+        if (argc < 5)
+        {
+            printf("è necessario fornire un numero di parole da generare!\n");
+            exit(1);
+        }
+
+        char punto[WORD_LENGHT] = ".";
+        if (argc < 6)
+        {
+            compito2(argv[2], argv[1], argv[4], punto);
+        }
+        else
+        {
+            compito2(argv[2], argv[1], argv[4], argv[5]);
+        }
+    }
+    else
+    {
+        printf("Bisogna indicare se eseguire il compito 1 oppure il compito 2.\nIl codice %s non è valido per nessun compito.", argv[3]);
+        exit(1);
+    }
+}
