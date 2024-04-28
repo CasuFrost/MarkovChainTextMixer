5faefee1a50a0b643370889233e6ea3e42c29928
casufrost <mcasu299@gmail.com>
2024-04-29 00:11:51 +0200

    nulla

--- MultiProcess/src/compito2.c
+++ MultiProcess/src/compito2.c
@@ -232,7 +232,6 @@ void compito2(char *input, char *output, char *numParole, char start[WORD_LENGHT
  parole), la seconda, crea un grafo pesato leggendo il CSV, e poi opera sul grafo, l'operazione di creazione del grafo è dispendiosa, ma poi sarà rapido scrivere sul file di output,
  per questo se le parole son poche verrà effettuato il processo con la prima versione, se invece son tante, verrà creato il grafo.*/
 {
-
     int Input_Graph_Pipe[2];
     int nextStep_Pipe[2];
     int endPipe[2];
@@ -252,108 +251,7 @@ void compito2(char *input, char *output, char *numParole, char start[WORD_LENGHT
     if (pid != 0)
     {
         // Figlio
-        char readbuffer[80];
-
-        close(Input_Graph_Pipe[0]);
-
-        int wordCounter = 0;
-        FILE *fp;
-        fp = fopen(input, "r"); /*Apro il file di input*/
-        char line[MAX_LINE_LENGHT];
-        while (fgets(line, MAX_LINE_LENGHT, fp)) // Leggo il file riga per riga
-        {
-
-            char tmp[WORD_LENGHT]; // Controllo la prima parola in questione
-
-            int i = 0;
-
-            while (i < strlen(line)) // Seleziono la prima parola della linea
-            {
-                if (line[i] == ',')
-                {
-                    tmp[i] = '\0';
-                    break;
-                }
-                tmp[i] = line[i];
-                i++;
-            }
-
-            // 'tmp' contiene la prima parola
-            write(Input_Graph_Pipe[1], tmp, (strlen(tmp) + 1));
-            write(endPipe[1], "aaa", 3);
-
-            read(nextStep_Pipe[0], readbuffer, sizeof(readbuffer)); // Il figlio attende l'ordine del padre per la prossima parola
-        }
-        write(endPipe[1], "end", 3);
-
-        // A questo punto del codice, il padre ha letto tutte le parole distinte nel file CSV, e le ha inviate al figlio
-        fseek(fp, 0, SEEK_SET); /* Torno all'inizio del file */
-
-        char nodeTo[WORD_LENGHT]; // Controllo la prima parola in questione
-        char tmp[WORD_LENGHT];
-        char freq[WORD_LENGHT];
-
-        while (fgets(line, MAX_LINE_LENGHT, fp)) // Leggo il file riga per riga
-        {
-
-            int i = 0;
-
-            while (i < strlen(line)) // Seleziono la prima parola della linea
-            {
-                if (line[i] == ',')
-                {
-                    nodeTo[i] = '\0';
-                    break;
-                }
-                nodeTo[i] = line[i];
-                i++;
-            }
-            // printf("%s\n", nodeTo);
-
-            i++;
-
-            int j = 0;
-
-            // Tutti gli archi partiranno da 'nodeTo'
-
-            while (i < strlen(line))
-            {
-                if (line[i] == ',')
-                {
-                    tmp[j] = '\0';
-                    // Dentro 'tmp' ho la parola
-                    j = 0;
-                    i++;
-                    while (line[i] != ',' && i < strlen(line))
-                    {
-                        freq[j] = line[i];
-                        i++;
-                        j++;
-                    }
-
-                    freq[j] = '\0';
-                    j = 0;
-
-                    char buffer[2 * WORD_LENGHT];
-                    sprintf(buffer, "%d %s %s\0", wordCounter, tmp, freq);
-                    write(endPipe[1], "aaa", 3);
-                    // printf("Invio la stringa : %s\n", buffer);
-                    write(Input_Graph_Pipe[1], buffer, (strlen(buffer) + 1));
-                    read(nextStep_Pipe[0], readbuffer, sizeof(readbuffer)); // Il figlio attende l'ordine del padre per la prossima parola
-                }
-                else
-                {
-                    tmp[j] = line[i];
-
-                    j++;
-                }
-                i++;
-            }
-            wordCounter++;
-        }
-        printf("scrivo fine\n");
-        write(endPipe[1], "end\0", 5);
-        write(Input_Graph_Pipe[1], "end", 3);
+        readFileAndSendWords(input, Input_Graph_Pipe, endPipe, nextStep_Pipe);
     }
     else
     {
@@ -376,9 +274,9 @@ void compito2(char *input, char *output, char *numParole, char start[WORD_LENGHT
 
             write(nextStep_Pipe[1], "nulla", 1); // Il padre notifica al figlio che può scrivere la prossima parola sulla PIPE
         }
-        // printGraph();
-        // printf("\n");
-
+        printGraph();
+        printf("\n");
+        return;
         // A questo punto del codice, il figlio ha ottenuto le parole distinte e creato i nodi del grafo (non gli archi)
         while (1)
         {
